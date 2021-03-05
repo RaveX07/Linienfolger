b3c8aa4f8bd0c742bdb92e9c2ae4aba8816c7dca
misterrabenklaue <stoepselklo@gmail.com>
2021-03-05 13:58:03 +0100

    Motor Namen geändert

--- src/Linienfolger.cpp
+++ src/Linienfolger.cpp
@@ -85,6 +85,40 @@ int checkDistanceSides(){
   distanceR = durationR*0.034/2; //distance in cm
 }
 
+int drive ()
+{
+  if ((LeftFromMiddle =0) & (RightFromMiddle =1))
+{
+    analogWrite(LEFT_PWM_PIN_FORWARD, 200);
+    analogWrite(RIGHT_PWM_PIN_FORWARD, 190);
+};
+  if ((LeftFromMiddle =1) & (RightFromMiddle =0))
+{
+    analogWrite(LEFT_PWM_PIN_FORWARD, 190);
+    analogWrite(RIGHT_PWM_PIN_FORWARD, 200);
+};
+  if ((HalfLeft =0) & (HalfRight =1))
+{
+    analogWrite(LEFT_PWM_PIN_FORWARD, 200);
+    analogWrite(RIGHT_PWM_PIN_FORWARD, 180);
+};
+  if ((HalfLeft =1) & (HalfRight =0))
+{
+    analogWrite(LEFT_PWM_PIN_FORWARD, 180);
+    analogWrite(RIGHT_PWM_PIN_FORWARD, 200)
+};
+  if ((Left =0) & (Right =1))
+{
+    analogWrite(LEFT_PWM_PIN_FORWARD, 200);
+    analogWrite(RIGHT_PWM_PIN_FORWARD, 170)
+};
+  if ((Left =1) & (Right =0))
+{
+    analogWrite(LEFT_PWM_PIN_FORWARD, 170);
+    analogWrite(RIGHT_PWM_PIN_FORWARD, 200)
+}
+}
+
 void setup() {
   Serial.begin(9600);
 
--- src/test_irsensor.cpp
+++ src/test_irsensor.cpp
@@ -2,31 +2,31 @@
 //ich gehe davon aus dass der normale Speed beim geradeausfahren 200 ist
 if ((LeftFromMiddle =0) & (RightFromMiddle =1))
 {
-    analogWrite(LeftMotor, 200);
-    analogWrite(RightMotor, 190)
+    analogWrite(LEFT_PWM_PIN_FORWARD, 200);
+    analogWrite(RIGHT_PWM_PIN_FORWARD, 190);
 };
 if ((LeftFromMiddle =1) & (RightFromMiddle =0))
 {
-    analogWrite(LeftMotor, 190)
-    analogWrite(RightMotor, 200)
+    analogWrite(LEFT_PWM_PIN_FORWARD, 190);
+    analogWrite(RIGHT_PWM_PIN_FORWARD, 200);
 };
 if ((HalfLeft =0) & (HalfRight =1))
 {
-    analogWrite(LeftMotor, 200);
-    analogWrite(RightMotor, 180)
+    analogWrite(LEFT_PWM_PIN_FORWARD, 200);
+    analogWrite(RIGHT_PWM_PIN_FORWARD, 180);
 };
 if ((HalfLeft =1) & (HalfRight =0))
 {
-    analogWrite(LeftMotor, 180);
-    analogWrite(RightMotor, 200)
+    analogWrite(LEFT_PWM_PIN_FORWARD, 180);
+    analogWrite(RIGHT_PWM_PIN_FORWARD, 200)
 };
 if ((Left =0) & (Right =1))
 {
-    analogWrite(LeftMotor, 200);
-    analogWrite(RightMotor, 170)
+    analogWrite(LEFT_PWM_PIN_FORWARD, 200);
+    analogWrite(RIGHT_PWM_PIN_FORWARD, 170)
 };
 if ((Left =1) & (Right =0))
 {
-    analogWrite(LeftMotor, 170);
-    analogWrite(RightMotor, 200)
+    analogWrite(LEFT_PWM_PIN_FORWARD, 170);
+    analogWrite(RIGHT_PWM_PIN_FORWARD, 200)
 }
