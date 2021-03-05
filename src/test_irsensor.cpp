f6e281036517929710bac3bea06981920312c0e5
misterrabenklaue <stoepselklo@gmail.com>
2021-03-05 12:30:27 +0100

    jetzt wohl richtige Motorcommands

--- src/test_irsensor.cpp
+++ src/test_irsensor.cpp
@@ -2,32 +2,32 @@
 //ich gehe davon aus dass der normale Speed beim geradeausfahren 200 ist
 if ((LeftFromMiddle =0) & (RightFromMiddle =))
 {
-    LeftMotor=200;
-    RightMotor=190
+    analogWrite(LeftMotor, 200);
+    analogWrite(RightMotor, 190)
     //So wurde das in dem Tutorial gemacht... Wir haben dafür die Library nicht installiert und ich kenn den korrekten Command und Motornamen nicht also ja...
 };
 if ((LeftFromMiddle =1) & (RightFromMiddle =0))
 {
-    LeftMotor=190
-    RightMotor=200
+    analogWrite(LeftMotor, 190)
+    analogWrite(RightMotor, 200)
 };
 if ((HalfLeft =0) & (HalfRight =1))
 {
-    LeftMoto=200;
-    RightMotor=180
+    analogWrite(LeftMotor, 200);
+    analogWrite(RightMotor, 180)
 };
 if ((HalfLeft =1) & (HalfRight =0))
 {
-    LeftMotor=180;
-    RightMotor=200
+    analogWrite(LeftMotor, 180);
+    analogWrite(RightMotor, 200)
 };
 if ((Left =0) & (Right =1))
 {
-    LeftMotor=200;
-    RightMotor=170
+    analogWrite(LeftMotor, 200);
+    analogWrite(RightMotor, 170)
 };
 if ((Left =1) & (Right =0))
 {
-    LeftMotor=170;
-    RightMotor=200
+    analogWrite(LeftMotor, 170);
+    analogWrite(RightMotor, 200)
 }
